73a8bd48075fdbadb3eba5e301c8f5a192e1cc5d
mendi160 <Meni Kuperman@DESKTOP-OERQ00U>
2020-11-01 01:13:49 +0200

    Add travel activity

--- app/src/main/java/com/project/travelme/Ui/AddTravelActivity.kt
+++ app/src/main/java/com/project/travelme/Ui/AddTravelActivity.kt
@@ -1,14 +1,12 @@
 package com.project.travelme.Ui
 
 import android.os.Bundle
-import android.text.Editable
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
 import com.project.travelme.R
-import com.project.travelme.R.id.fixed
 import com.project.travelme.R.id.passengers
 
 class AddTravelActivity : AppCompatActivity() {
@@ -16,31 +14,22 @@ class AddTravelActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_travel)
         Toast.makeText(this, "hi", Toast.LENGTH_LONG).show()
-       // findViewById<EditText>(passengers).setText("0")
-        findViewById<EditText>(passengers).addTextChangedListener {
-           val  text=findViewById<EditText>(passengers).text.toString()
-
-
-
-            if ( text==""||text.toInt()< 0)
-                findViewById<EditText>(passengers).setText("0")
+        val passengersEditText: EditText = findViewById<EditText>(passengers) as EditText
+        passengersEditText.addTextChangedListener {
+            val text = passengersEditText.text.toString()
+            if (text == "" || text.toInt() < 0)
+                passengersEditText.setText("0")
         }
     }
 
-
     fun less(view: View) {
-
-
-            findViewById<EditText>(passengers).setText(
-                (findViewById<EditText>(passengers).text.toString().toInt() - 1).toString()
-            )
-//
-
-
+        findViewById<EditText>(passengers).setText(
+            (findViewById<EditText>(passengers).text.toString().toInt() - 1).toString()
+        )
     }
 
     fun more(view: View) {
-        if (  findViewById<EditText>(passengers).text.toString()==""  )
+        if (findViewById<EditText>(passengers).text.toString() == "")
             findViewById<EditText>(passengers).setText("0")
         findViewById<EditText>(passengers).setText(
             (findViewById<EditText>(passengers).text.toString().toInt() + 1).toString()
