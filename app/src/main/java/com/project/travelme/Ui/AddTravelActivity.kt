f3e977dc00bd2909c80f18263c14abfcf96d36a0
mendi160 <Meni Kuperman@DESKTOP-OERQ00U>
2020-10-31 21:23:23 +0200

    package

---
+++ app/src/main/java/com/project/travelme/Ui/AddTravelActivity.kt
@@ -0,0 +1,12 @@
+package com.project.travelme.Ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.project.travelme.R
+
+class AddTravelActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_travel)
+    }
+}
