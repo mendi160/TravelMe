2cf4d2ac8c6bd9ce4a82e6671d54dea348e2478a
mendi160 <Meni Kuperman@DESKTOP-OERQ00U>
2020-11-02 10:05:04 +0200

    Add travel activity

---
+++ app/src/main/java/com/project/travelme/Data/TravelDataSource.kt
@@ -0,0 +1,4 @@
+package com.project.travelme.Data
+
+class TravelDataSource {
+}
--- app/src/main/java/com/project/travelme/Ui/AddTravelActivity.kt
+++ app/src/main/java/com/project/travelme/Ui/AddTravelActivity.kt
@@ -16,7 +16,6 @@ class AddTravelActivity : AppCompatActivity() {
     private lateinit var etPassengers: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_add_travel)
         Toast.makeText(this, "hi", Toast.LENGTH_LONG).show()
         etPassengers = findViewById<EditText>(passengers)
@@ -29,8 +28,6 @@ class AddTravelActivity : AppCompatActivity() {
             findViewById<LinearLayout>(R.id.departureDate).findViewById<Button>(R.id.pickDateBtn)
                 .setOnClickListener {
                     pickDate(findViewById<LinearLayout>(R.id.departureDate).findViewById<TextView>(R.id.dateTextView))
-
-
                 }
         var bReturn =
             findViewById<LinearLayout>(R.id.returnDate).findViewById<Button>(R.id.pickDateBtn)
@@ -52,7 +49,6 @@ class AddTravelActivity : AppCompatActivity() {
     }
 
     fun more(view: View) {
-
         if (etPassengers.text.toString() == "")
             etPassengers.setText("1")
         else {
@@ -64,9 +60,6 @@ class AddTravelActivity : AppCompatActivity() {
     }
 
     fun pickDate(textV: TextView) {
-
-
-
         val c = Calendar.getInstance()
         val year = c.get(Calendar.YEAR)
         val month = c.get(Calendar.MONTH).toInt()
@@ -81,23 +74,24 @@ class AddTravelActivity : AppCompatActivity() {
             month,
             day
         )
-        dpd.datePicker.minDate=System.currentTimeMillis()-1000
+        dpd.datePicker.minDate = System.currentTimeMillis() - 1000
         dpd.show()
     }
-    fun showDialog(){
+
+    fun showDialog() {
         val dialog = Dialog(this)
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
         dialog.setTitle("Address")
         dialog.setCancelable(true)
         dialog.setContentView(R.layout.address_form)
-       // val body = dialog.findViewById(R.id.body) as TextView
-       // body.text = title
-       // val yesBtn = dialog.findViewById(R.id.yesBtn) as Button
-       // val noBtn = dialog.findViewById(R.id.noBtn) as TextView
-       // yesBtn.setOnClickListener {
-       //     dialog.dismiss()
-      //  }
-       // noBtn.setOnClickListener { dialog.dismiss() }
+        // val body = dialog.findViewById(R.id.body) as TextView
+        // body.text = title
+        // val yesBtn = dialog.findViewById(R.id.yesBtn) as Button
+        // val noBtn = dialog.findViewById(R.id.noBtn) as TextView
+        // yesBtn.setOnClickListener {
+        //     dialog.dismiss()
+        //  }
+        // noBtn.setOnClickListener { dialog.dismiss() }
         dialog.show()
     }
 }
