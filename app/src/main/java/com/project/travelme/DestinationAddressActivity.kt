72aa0d509b896fb2914fbecdc1ea2435e8736bac
mendi160 <Meni Kuperman@DESKTOP-OERQ00U>
2020-11-09 12:44:20 +0200

    Add travel activity

--- app/src/main/java/com/project/travelme/DestinationAddressActivity.kt
+++ app/src/main/java/com/project/travelme/DestinationAddressActivity.kt
@@ -3,13 +3,25 @@ package com.project.travelme
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import com.project.travelme.Utils.Address
 
 class DestinationAddressActivity : AppCompatActivity() {
+    lateinit var address: ArrayAdapter<Address>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_destination_address)
         this.setFinishOnTouchOutside(false)
+        var addressArray = arrayOf(Address("Tel-Aviv", "alanbi", 12))
+
+        var mListView = findViewById<ListView>(R.id.lvAddress)
+        address = ArrayAdapter(this,addressArray)
+            this
+            android.R.layout.simple_list_item_1, addressArray
+        )
+        mListView.adapter = address
     }
 
     fun showAddAddressDialog(view: View) {
