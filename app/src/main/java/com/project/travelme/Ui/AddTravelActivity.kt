8bc834c1fedf60101e2dcf2cb18c430321fd4395
mendi160 <Meni Kuperman@DESKTOP-OERQ00U>
2021-01-16 20:56:44 +0200

    The bug that happened when no destination address was selected has been fixed

--- app/src/main/java/com/project/travelme/Ui/AddTravelActivity.kt
+++ app/src/main/java/com/project/travelme/Ui/AddTravelActivity.kt
@@ -40,7 +40,7 @@ class AddTravelActivity : AppCompatActivity() {
     private lateinit var bDestination: Button
     private lateinit var bSave: Button
     private lateinit var dialog: Dialog
-    private var debug = false
+    private var debug = true
 
     private var isSourceAddress by Delegates.notNull<Boolean>()
 
